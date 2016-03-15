a2ee57b70636f397fc4215df98f51848fe6266d1
xiang <xiang9311@sina.cn>
2016-03-15 17:48:03 +0800

    设置编辑器宽度

--- TimeHouseServer/WebEditor/constant/constant.py
+++ TimeHouseServer/WebEditor/constant/constant.py
@@ -2,7 +2,7 @@ __author__ = '祥祥'
 
 import time
 
-NAME_TIME_FORMAT = '%Y%m%d%H%M%S'
+NAME_TIME_FORMAT = '%Y-%m-%d%H%M%S'
 
 def getHtmlFileName(headName):
     return headName + time.strftime(NAME_TIME_FORMAT, time.localtime()) + '.html'
